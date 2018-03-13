7badbfa3fa067f6d2ba11704d94a026a40c93655
LukeXie <luke.xie@yahoo.com>
2018-03-13 13:33:03 +0800

    Update __init__.py

--- sphinx_autobuild/__init__.py
+++ sphinx_autobuild/__init__.py
@@ -168,7 +168,7 @@ class SphinxBuilder(object):
         sys.stdout.write('-' * (81 - len(pre)))
         sys.stdout.write('\n')
 
-        args = ['sphinx'] + self._args
+        args = [sys.executable, '-msphinx'] + self._args
         if pty:
             master, slave = pty.openpty()
             stdout = os.fdopen(master)
